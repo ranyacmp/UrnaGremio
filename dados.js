1922984c34f98437b37a53603ec56f87b822c86f
Ranya Campanario <105180946+ranyacmp@users.noreply.github.com>
2023-06-29 12:53:59 -0300

    Mudando fotos

--- dados.js
+++ dados.js
@@ -6,7 +6,7 @@ var turmas={
             sala: "GREMIO",         
             nome: ["Branca","Roxa", "Azul"],   
             numero: [1,2,3] , 
-            foto:["FOTOS/cor.png","FOTOS/roxo.jpg","FOTOS/azul.jpg"]
+            foto:["FOTOS/Braca.png","FOTOS/Roxa.png","FOTOS/Azul.png"]
             }
             
             
